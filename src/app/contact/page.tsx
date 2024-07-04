796ad7effc9039e10d2e7faa98cb55cd9d0d3106
ibrahimox <ibrahimkarmouna29@gmail.com>
2024-07-04 12:52:02 +0100

    update the contact page to make it send msgs

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,30 +1,54 @@
-import React, { FC } from "react";
+"use client"
+import React, { useState } from "react";
 import SocialsList from "@/shared/SocialsList/SocialsList";
 import Label from "@/components/Label/Label";
 import Input from "@/shared/Input/Input";
 import Textarea from "@/shared/Textarea/Textarea";
 import ButtonPrimary from "@/shared/Button/ButtonPrimary";
-import BackgroundSection from "@/components/BackgroundSection/BackgroundSection";
-import SectionPromo1 from "@/components/SectionPromo1";
+import emailjs from "@emailjs/browser";
+import { toast } from "sonner";
 
 const info = [
   {
     title: "🗺 ADDRESS",
-    desc: "Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter",
+    desc: "Hay saada 1 ,Driouch",
   },
   {
     title: "💌 EMAIL",
-    desc: "nc.example@example.com",
+    desc: "contact@Zeemarke.ma",
   },
   {
     title: "☎ PHONE",
-    desc: "000-123-456-7890",
+    desc: "+212-673-712847",
   },
 ];
 
-const PageContact = ({}) => {
+const PageContact = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const sendEmail = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      setIsSubmitting(true);
+      await emailjs.sendForm(
+        "service_ukha5rj",  // Replace with your service ID
+        "template_48q0skr", // Replace with your template ID
+        e.target,
+        "D6rvZ-4VTYBkI0nkJ" // Replace with your user ID
+      );
+      toast.success("Email Sent Successfully");
+      e.target.reset();
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to send email");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <div className={`nc-PageContact overflow-hidden`}>
+    <div className="nc-PageContact overflow-hidden mb-7">
       <div className="">
         <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
           Contact
@@ -50,32 +74,35 @@ const PageContact = ({}) => {
               </div>
             </div>
             <div>
-              <form className="grid grid-cols-1 gap-6" action="#" method="post">
+              <form className="grid grid-cols-1 gap-6" onSubmit={sendEmail}>
                 <label className="block">
                   <Label>Full name</Label>
-
                   <Input
-                    placeholder="Example Doe"
+                    placeholder="Example brahim"
                     type="text"
+                    name="name"
                     className="mt-1"
+                    required
                   />
                 </label>
                 <label className="block">
                   <Label>Email address</Label>
-
                   <Input
                     type="email"
                     placeholder="example@example.com"
+                    name="email"
                     className="mt-1"
+                    required
                   />
                 </label>
                 <label className="block">
                   <Label>Message</Label>
-
-                  <Textarea className="mt-1" rows={6} />
+                  <Textarea className="mt-1" rows={6} name="message" required />
                 </label>
                 <div>
-                  <ButtonPrimary type="submit">Send Message</ButtonPrimary>
+                  <ButtonPrimary type="submit" disabled={isSubmitting}>
+                    {isSubmitting ? "Sending..." : "Send Message"}
+                  </ButtonPrimary>
                 </div>
               </form>
             </div>
@@ -84,12 +111,12 @@ const PageContact = ({}) => {
       </div>
 
       {/* OTHER SECTIONS */}
-      <div className="container">
+      {/* <div className="container">
         <div className="relative my-24 lg:my-32 py-24 lg:py-32">
           <BackgroundSection />
           <SectionPromo1 />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
